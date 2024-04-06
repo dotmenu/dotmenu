7acb4d8c3b13b920ae8dee3a36457ca20fac1e19
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-06 14:28:11 -0500

    Corrected the play option in the Hangman sample.

---
+++ Hangman/Gameplay/PlayOption.cs
@@ -0,0 +1,18 @@
+﻿using dotmenu;
+
+namespace Hangman.Gameplay;
+
+/// <summary>
+/// Represents an option <see cref="Option"/> to start the game.
+/// </summary>
+public sealed class PlayOption()
+    : Option(
+        text: "Play",
+        action: StartGame)
+{
+    private static void StartGame()
+    {
+        var guessScene = new GuessScene();
+        guessScene.Start();
+    }
+}
