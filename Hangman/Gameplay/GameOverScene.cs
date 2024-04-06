518a5efe313703d5b5c92d9482541feec7c84df8
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-06 14:27:39 -0500

    Created a game over scene for the Hangman sample.

---
+++ Hangman/Gameplay/GameOverScene.cs
@@ -0,0 +1,16 @@
+﻿namespace Hangman.Gameplay;
+
+/// <summary>
+/// Represents the scene that is displayed when the game is over.
+/// </summary>
+internal sealed class GameOverScene(string word)
+    : Scene
+{
+    /// <inheritdoc />
+    protected override void Update()
+    {
+        WriteCentered("Game Over!");
+        WriteLowerLeft($"The word was: {word}");
+        End();
+    }
+}
