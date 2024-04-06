46e5466b0647313d457a450288513dc948c0577d
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-06 14:27:08 -0500

    Created a scene object for the Hangman sample.

---
+++ Hangman/Scene.cs
@@ -0,0 +1,56 @@
+﻿namespace Hangman;
+
+/// <summary>
+/// Represents a scene in the game.
+/// </summary>
+public abstract class Scene
+{
+    private bool _running;
+    
+    /// <summary>
+    /// Starts the scene.
+    /// </summary>
+    public void Start()
+    {
+        if (_running)
+            return;
+        
+        _running = true;
+        while (_running)
+            Update();
+    }
+
+    /// <summary>
+    /// Ends the scene.
+    /// </summary>
+    public void End() =>
+        _running = false;
+    
+    /// <summary>
+    /// Updates the scene.
+    /// </summary>
+    protected abstract void Update();
+
+    /// <summary>
+    /// Renders the specified text centered on the console.
+    /// </summary>
+    /// <param name="text">The text to render.</param>
+    protected static void WriteCentered(string text)
+    {
+        var verticalPadding = Console.WindowHeight / 2;
+        var horizontalPadding = (Console.WindowWidth - text.Length) / 2;
+        Console.SetCursorPosition(horizontalPadding, verticalPadding);
+        Console.Write(text);
+    }
+    
+    /// <summary>
+    /// Renders the specified text in the lower-left corner of the console.
+    /// </summary>
+    /// <param name="text">The text to render.</param>
+    protected static void WriteLowerLeft(string text)
+    {
+        var consoleHeight = Console.WindowHeight;
+        Console.SetCursorPosition(0, consoleHeight - 1);
+        Console.Write(text);
+    }
+}
