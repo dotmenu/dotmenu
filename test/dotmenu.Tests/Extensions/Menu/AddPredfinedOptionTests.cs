a11a40ef54f6463d2a0b9f31232b635c1d53c1a8
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-06 11:40:00 -0500

    ✨ WIP: Adding support for unit testing.

---
+++ test/dotmenu.Tests/Extensions/Menu/AddPredfinedOptionTests.cs
@@ -0,0 +1,26 @@
+﻿namespace dotmenu.Tests.Extensions;
+
+public class AddPredfinedOptionTests
+{
+    [Fact]
+    public void AddPredfinedOption_WithNullMenu_ThrowsArgumentNullException()
+    {
+        Menu? menu = null;
+        Assert.Throws<ArgumentNullException>(TestAction);
+        return;
+        
+        void TestAction() =>
+            menu!.AddOption<PredefinedOption>();
+    }
+    
+    [Fact]
+    public void AddPredfinedOption_AddsOptionToMenu()
+    {
+        var menu = new Menu();
+        menu.AddOption<PredefinedOption>();
+        Assert.Single(menu.options);
+    }
+
+    private sealed class PredefinedOption()
+        : Option(text: "sample");
+}
---
+++ test/dotmenu.Tests/Extensions/Option/ValidateTests.cs
@@ -0,0 +1,57 @@
+﻿namespace dotmenu.Tests.Extensions;
+
+public class ValidateTests
+{
+    [Fact]
+    public void Validate_WithNullOption_ThrowsArgumentNullException()
+    {
+        Option? option = null;
+        Assert.Throws<ArgumentNullException>(TestAction);
+        return;
+        
+        void TestAction() =>
+            option!.Validate();
+    }
+    
+    [Theory]
+    [InlineData(null!)]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("\t")]
+    public void Validate_WithInvalidText_ThrowsArgumentException(string? text)
+    {
+        var option = new Option(text);
+        Assert.Throws<ArgumentException>(TestAction);
+        return;
+        
+        void TestAction() =>
+            option.Validate();
+    }
+    
+    [Fact]
+    public void Validate_WithValidText_DoesNotThrow()
+    {
+        var option = new Option("text");
+        option.Validate();
+    }
+    
+    [Fact]
+    public void Validate_WithNullTextFunction_ThrowsArgumentNullException()
+    {
+        var option = new Option(null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null);
+        
+        Assert.Throws<ArgumentNullException>(TestAction);
+        return;
+        
+        void TestAction() =>
+            option.Validate();
+    }
+}
