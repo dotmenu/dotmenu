b8478379c068f2bd16fb6ddc98d4b6275414cef0
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-06 14:26:42 -0500

    Added a credits scene in the Hangman sample.

---
+++ Hangman/Credits/CreditsScene.cs
@@ -0,0 +1,15 @@
+﻿namespace Hangman.Credits;
+
+/// <summary>
+/// Represents the scene that is displayed when the credits are shown.
+/// </summary>
+internal sealed class CreditsScene
+    : Scene
+{
+    /// <inheritdoc />
+    protected override void Update()
+    {
+        WriteCentered("Written By: tacosontitan");
+        End();
+    }
+}
