88fb0208a4f26dc93f5d6698ac8d5a7143d3b1c8
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-07 11:02:51 -0500

    Created a menu element interface.

--- src/dotmenu/IMenuOption.cs
+++ src/dotmenu/IMenuOption.cs
@@ -4,22 +4,8 @@
 /// Represents a menu option.
 /// </summary>
 public interface IMenuOption
+    : IMenuElement
 {
-    /// <summary>
-    /// Gets or sets whether or not this menu option is visible.
-    /// </summary>
-    bool Visible { get; set; }
-    
-    /// <summary>
-    /// Gets or sets whether or not this menu option is enabled.
-    /// </summary>
-    bool Enabled { get; set; }
-
-    /// <summary>
-    /// Gets or sets the text to display for this menu option.
-    /// </summary>
-    string Text { get; set; }
-
     /// <summary>
     /// Invokes the action associated with this menu option.
     /// </summary>
