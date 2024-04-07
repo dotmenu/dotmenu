e3fdb31a11e9baf97af602a8abc89ff4aafafa60
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-07 15:00:24 -0500

    Moved menu interface.

--- src/dotmenu/Menu/IMenu.cs
+++ src/dotmenu/Menu/IMenu.cs
@@ -1,10 +1,17 @@
-﻿namespace dotmenu;
+﻿using System.Collections.Immutable;
+
+namespace dotmenu;
 
 /// <summary>
 /// Represents a menu.
 /// </summary>
 public interface IMenu
 {
+    /// <summary>
+    /// Gets the elements of the menu.
+    /// </summary>
+    ImmutableArray<IMenuElement> Elements { get; }
+    
     /// <summary>
     /// Runs the menu.
     /// </summary>
