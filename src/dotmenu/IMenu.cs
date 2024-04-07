5a07abcd145144ffa5d725ab1615d280c44feabb
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-07 09:08:41 -0500

    Created a simple menu interface.

---
+++ src/dotmenu/IMenu.cs
@@ -0,0 +1,13 @@
+﻿namespace dotmenu;
+
+/// <summary>
+/// Represents a menu.
+/// </summary>
+public interface IMenu
+{
+    /// <summary>
+    /// Runs the menu.
+    /// </summary>
+    /// <returns>The index of the option selected by the user.</returns>
+    void Run();
+}
