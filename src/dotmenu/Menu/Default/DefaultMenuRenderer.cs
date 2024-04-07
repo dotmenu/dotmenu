165dd0ac9ca777749f3dd2b19ef9525e41eb2e41
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-07 15:03:58 -0500

    Created default menu builder support.

---
+++ src/dotmenu/Menu/Default/DefaultMenuRenderer.cs
@@ -0,0 +1,99 @@
+﻿using System.Text;
+
+namespace dotmenu.Graphics;
+
+/// <summary>
+/// Represents a default menu renderer.
+/// </summary>
+internal sealed class DefaultMenuRenderer
+    : IMenuRenderer
+{
+    private readonly StringBuilder _renderer = new();
+    
+    /// <summary>
+    /// Creates a new instance of <see cref="DefaultMenuRenderer"/>.
+    /// </summary>
+    /// <param name="prefix">The prefix for unselected menu options.</param>
+    /// <param name="selector">The prefix for selected menu options.</param>
+    /// <param name="theme">The theme to use.</param>
+    public DefaultMenuRenderer(
+        string? selector,
+        string? prefix,
+        ITheme? theme)
+    {
+        Selector = selector;
+        Prefix = prefix;
+        Theme = theme;
+    }
+    
+    /// <inheritdoc />
+    public string? Selector { get; set; }
+
+    /// <inheritdoc />
+    public string? Prefix { get; set; }
+
+    /// <inheritdoc />
+    public ITheme? Theme { get; set; }
+
+    /// <inheritdoc />
+    public void Render(IMenu menu)
+    {
+        Console.Clear();
+        _renderer.Clear();
+        
+        RenderTitle(menu);
+        RenderOptions(menu);
+
+        Console.SetCursorPosition(0, 0);
+        Console.Write(_renderer.ToString());
+    }
+
+    private void RenderTitle(IMenu menu)
+    {
+        var titleElement = menu.Elements.SingleOrDefault(IsTitleElement);
+        RenderElement(titleElement, usePrefix: false);
+        
+        static bool IsTitleElement(IMenuElement element)
+            => element is MenuTitle;
+    }
+    
+    private void RenderOptions(IMenu menu)
+    {
+        foreach (var element in menu.Elements.OfType<IMenuOption>())
+        {
+            if (element is not { Visible: true })
+                continue;
+
+            RenderElement(element);
+        }
+    }
+
+    private void RenderElement(IMenuElement? element, bool usePrefix = true)
+    {
+        if (element is not { Visible: true })
+            return;
+
+        var prefix = usePrefix ? Prefix : string.Empty;
+        if (usePrefix && element is IMenuOption option)
+            prefix = option.Selected ? Selector : Prefix;
+        
+        var line = $"{prefix} {element.Text}".Trim();
+        var formattedLine = FormatLine(line);
+        _renderer.AppendLine(formattedLine);
+    }
+    
+    private string FormatLine(string text)
+    {
+        const string colorEscapeCode = "\x1b[38;2;{0};{1};{2}m\x1b[48;2;{3};{4};{5}m{6}\x1b[0m";
+        var (foregroundRed, foregroundGreen, foregroundBlue) = Theme?.Foreground ?? ThemeColor.White;
+        var (backgroundRed, backgroundGreen, backgroundBlue) = Theme?.Background ?? ThemeColor.Black;
+        var coloredText = string.Format(colorEscapeCode,
+            foregroundRed, foregroundGreen, foregroundBlue,
+            backgroundRed, backgroundGreen, backgroundBlue,
+            text);
+        
+        var paddingLength = Math.Max(0, Console.BufferWidth - text.Length);
+        var padding = new string(' ', paddingLength);
+        return $"{coloredText}{padding}";
+    }
+}
---
+++ src/dotmenu/Menu/Menu.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Immutable;
+
+using dotmenu.Formatting;
+using dotmenu.Graphics;
+
+namespace dotmenu;
+
+/// <summary>
+/// Represents a menu.
+/// </summary>
+public sealed partial class Menu
+    : IMenu
+{
+    private readonly IMenuConfiguration _configuration;
+    private readonly IMenuRenderer _renderer;
+    private int _selectedIndex;
+
+    /// <summary>
+    /// Creates a new menu instance.
+    /// </summary>
+    /// <param name="configuration">The configuration to use.</param>
+    /// <param name="renderer">The renderer to use.</param>
+    /// <param name="elements">The elements of the menu.</param>
+    internal Menu(
+        IMenuConfiguration configuration,
+        IMenuRenderer renderer,
+        ImmutableArray<IMenuElement> elements)
+    {
+        _configuration = configuration;
+        _renderer = renderer;
+        Elements = elements;
+    }
+    
+    /// <inheritdoc />
+    public ImmutableArray<IMenuElement> Elements { get; }
+
+    /// <inheritdoc />
+    public void Run()
+    {
+        if (!AnsiDetector.SupportsAnsi)
+        {
+            Console.WriteLine("Please use a terminal that supports ANSI escape codes.");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            
+            Environment.Exit(2);
+            return;
+        }
+
+        Initialize();
+        CancellationToken cancellationToken = default;
+        UpdateMenu().Wait(cancellationToken);
+        
+        async Task UpdateMenu()
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(_configuration.RefreshRate);
+                if (!TryGetInput())
+                    continue;
+            
+                _renderer.Render(this);
+            }
+        }
+    }
+
+    private void Initialize()
+    {
+        var firstOption = Elements.OfType<IMenuOption>().FirstOrDefault();
+        if (firstOption is null)
+        {
+            Console.WriteLine("No options found.");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            
+            Environment.Exit(3);
+            return;
+        }
+        
+        firstOption.Selected = true;
+        _renderer.Render(this);
+    }
+
+    private bool TryGetInput()
+    {
+        var key = Console.ReadKey(intercept: true).Key;
+        switch (key)
+        {
+            case ConsoleKey.UpArrow:
+                MoveSelection(-1);
+                return true;
+            case ConsoleKey.DownArrow:
+                MoveSelection(1);
+                return true;
+            case ConsoleKey.Enter:
+                InvokeSelectedOption();
+                return true;
+            default:
+                return false;
+        }
+    }
+    
+    private void MoveSelection(int offset)
+    {
+        var options = Elements.OfType<IMenuOption>().ToArray();
+        var selectedOption = options[_selectedIndex];
+        selectedOption.Selected = false;
+
+        _selectedIndex += offset;
+        if (_selectedIndex < 0)
+            _selectedIndex = options.Length - 1;
+        else if (_selectedIndex >= options.Length)
+            _selectedIndex = 0;
+
+        selectedOption = options[_selectedIndex];
+        selectedOption.Selected = true;
+    }
+    
+    private void InvokeSelectedOption()
+    {
+        var selectedOption = Elements.OfType<IMenuOption>().ElementAtOrDefault(_selectedIndex);
+        selectedOption?.Invoke();
+    }
+}
