2e2ea13c98f1959f1c7acdbbda8ef1578128d63d
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-07 15:00:50 -0500

    Created menu renderer interface.

---
+++ src/dotmenu/Graphics/IMenuRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace dotmenu.Graphics;
+
+/// <summary>
+/// Defines methods for rendering menu elements.
+/// </summary>
+public interface IMenuRenderer
+{
+    /// <summary>
+    /// Gets or sets the prefix for unselected menu options.
+    /// </summary>
+    string? Prefix { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the prefix for selected menu options.
+    /// </summary>
+    string? Selector { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the theme of the menu.
+    /// </summary>
+    ITheme? Theme { get; set; }
+    
+    /// <summary>
+    /// Renders the specified menu.
+    /// </summary>
+    void Render(IMenu menu);
+}
