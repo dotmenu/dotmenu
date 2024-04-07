d6cd43ca7df000ffde8460dc88596af1d9723663
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-07 09:30:15 -0500

    Created a simple theme interface.

---
+++ src/dotmenu/Graphics/ITheme.cs
@@ -0,0 +1,27 @@
+﻿namespace dotmenu.Graphics;
+
+/// <summary>
+/// Represents a theme for menus.
+/// </summary>
+public interface ITheme
+{
+    /// <summary>
+    /// Gets the natural foreground color for the menu.
+    /// </summary>
+    ThemeColor Foreground { get; }
+
+    /// <summary>
+    /// Gets the natural background color for the menu.
+    /// </summary>
+    ThemeColor Background { get; }
+
+    /// <summary>
+    /// Gets the selection foreground color for menu options.
+    /// </summary>
+    ThemeColor SelectionForeground { get; }
+    
+    /// <summary>
+    /// Gets the selection background color for menu options.
+    /// </summary>
+    ThemeColor SelectionBackground { get; }
+}
