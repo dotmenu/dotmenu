2bd1365499b2b121d6e626f456933d36e0601e55
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-07 19:10:36 -0500

    Simplified the default menu implementations to leverage new abstractions.

--- src/dotmenu/Menu/Default/DefaultMenuBuilder.cs
+++ src/dotmenu/Menu/Default/DefaultMenuBuilder.cs
@@ -1,76 +1,17 @@
-﻿using System.Collections.Immutable;
-
-using dotmenu.Graphics;
-using dotmenu.Graphics.Themes;
+﻿using dotmenu.Graphics;
 
 namespace dotmenu;
 
-public class DefaultMenuBuilder
-    : IMenuBuilder
+/// <summary>
+/// Represents the default implementation of a menu builder.
+/// </summary>
+internal sealed class DefaultMenuBuilder
+    : MenuBuilder
 {
-    private readonly IList<IMenuElement> _elements = new List<IMenuElement>(capacity: 1);
-    private readonly IMenuConfiguration _configuration = new MenuConfiguration();
-    private ITheme _theme = new DefaultTheme();
-    private string _selector = ">";
-    private string? _prefix;
-    private string? _title;
-
     /// <inheritdoc />
-    public IMenuBuilder SetTitle(string title)
-    {
-        _title = title;
-        return this;
-    }
-
-    /// <inheritdoc />
-    public IMenuBuilder SetPrefix(string prefix)
-    {
-        _prefix = prefix;
-        return this;
-    }
-
-    /// <inheritdoc />
-    public IMenuBuilder SetSelector(string selector)
-    {
-        _selector = selector;
-        return this;
-    }
-
-    /// <inheritdoc />
-    public IMenuBuilder SetRefreshRate(TimeSpan refreshRate)
-    {
-        _configuration.RefreshRate = refreshRate;
-        return this;
-    }
-
-    /// <inheritdoc />
-    public IMenuBuilder AddOption(IMenuOption option)
-    {
-        _elements.Add(option);
-        return this;
-    }
-
-    /// <inheritdoc />
-    public IMenuBuilder SetTheme(ITheme theme)
-    {
-        _theme = theme;
-        return this;
-    }
-
-    /// <inheritdoc />
-    public IMenu Build()
-    {
-        if (!string.IsNullOrWhiteSpace(_title))
-        {
-            var title = new MenuTitle(_title);
-            _elements.Insert(0, title);
-        }
-        
-        var elements = _elements.ToImmutableArray();
-        var renderer = new DefaultMenuRenderer(_selector, _prefix, _theme);
-        return new Menu(
-            _configuration,
-            renderer,
-            elements);
-    }
+    protected override IMenuRenderer CreateRenderer(
+        string selector,
+        string? prefix,
+        ITheme theme) =>
+        new DefaultMenuRenderer(selector, prefix, theme);
 }
--- src/dotmenu/Menu/Default/DefaultMenuRenderer.cs
+++ src/dotmenu/Menu/Default/DefaultMenuRenderer.cs
@@ -1,113 +1,39 @@
 ﻿using System.Text;
 
+using dotmenu.Extensions;
+
 namespace dotmenu.Graphics;
 
 /// <summary>
 /// Represents a default menu renderer.
 /// </summary>
 internal sealed class DefaultMenuRenderer
-    : IMenuRenderer
+    : ConsoleRenderer
 {
-    private readonly StringBuilder _renderer = new();
-    
-    /// <summary>
-    /// Creates a new instance of <see cref="DefaultMenuRenderer"/>.
-    /// </summary>
-    /// <param name="prefix">The prefix for unselected menu options.</param>
-    /// <param name="selector">The prefix for selected menu options.</param>
-    /// <param name="theme">The theme to use.</param>
+    /// <inheritdoc />
     public DefaultMenuRenderer(
         string? selector,
         string? prefix,
         ITheme? theme)
+        : base(selector, prefix, theme)
     {
-        Selector = selector;
-        Prefix = prefix;
-        Theme = theme;
     }
-    
-    /// <inheritdoc />
-    public string? Selector { get; set; }
 
     /// <inheritdoc />
-    public string? Prefix { get; set; }
+    protected override void RenderTitle(IMenuElement title)
+    {
+        var color = Theme?.GetAnsiColor(title);
+        AnsiConsole.WriteLine(title.Text, color);
+    }
 
     /// <inheritdoc />
-    public ITheme? Theme { get; set; }
-
-    /// <inheritdoc />
-    public void Render(IMenu menu)
+    protected override void RenderOption(IMenuOption option)
     {
-        Console.Clear();
-        _renderer.Clear();
-        
-        RenderTitle(menu);
-        RenderOptions(menu);
+        var color = Theme?.GetAnsiColor(option);
+        var text = option.Selected
+            ? $"{Selector} {option.Text}"
+            : $"{Prefix} {option.Text}";
 
-        Console.SetCursorPosition(0, 0);
-        Console.Write(_renderer.ToString());
-    }
-
-    private void RenderTitle(IMenu menu)
-    {
-        var titleElement = menu.Elements.SingleOrDefault(IsTitleElement);
-        RenderElement(titleElement, usePrefix: false);
-        
-        static bool IsTitleElement(IMenuElement element)
-            => element is MenuTitle;
-    }
-    
-    private void RenderOptions(IMenu menu)
-    {
-        foreach (var element in menu.Elements.OfType<IMenuOption>())
-        {
-            if (element is not { Visible: true })
-                continue;
-
-            RenderElement(element);
-        }
-    }
-
-    private void RenderElement(
-        IMenuElement? element,
-        bool usePrefix = true)
-    {
-        if (element is not { Visible: true })
-            return;
-
-        var useSelectionColors = false;
-        var prefix = usePrefix ? Prefix : string.Empty;
-        if (usePrefix && element is IMenuOption option)
-        {
-            useSelectionColors = option.Selected;
-            prefix = option.Selected ? Selector : Prefix;
-        }
-
-        var line = $"{prefix} {element.Text}".Trim();
-        var formattedLine = FormatLine(line, useSelectionColors);
-        _renderer.AppendLine(formattedLine);
-    }
-    
-    private string FormatLine(
-        string text,
-        bool useSelectionColors)
-    {
-        var (foregroundRed, foregroundGreen, foregroundBlue) = useSelectionColors
-            ? Theme?.SelectionForeground ?? ThemeColor.White
-            : Theme?.Foreground ?? ThemeColor.White;
-        
-        var (backgroundRed, backgroundGreen, backgroundBlue) = useSelectionColors
-            ? Theme?.SelectionBackground ?? ThemeColor.Black
-            : Theme?.Background ?? ThemeColor.Black;
-        
-        const string colorEscapeCode = "\x1b[38;2;{0};{1};{2}m\x1b[48;2;{3};{4};{5}m{6}\x1b[0m";
-        var coloredText = string.Format(colorEscapeCode,
-            foregroundRed, foregroundGreen, foregroundBlue,
-            backgroundRed, backgroundGreen, backgroundBlue,
-            text);
-        
-        var paddingLength = Math.Max(0, Console.BufferWidth - text.Length);
-        var padding = new string(' ', paddingLength);
-        return $"{coloredText}{padding}";
+        AnsiConsole.WriteLine(text, color);
     }
 }
