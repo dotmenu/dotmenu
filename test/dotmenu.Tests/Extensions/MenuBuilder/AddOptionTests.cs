c2a10a1d408148afa854b91347df30f91675f720
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-07 20:56:18 -0500

    Corrected unit tests.

---
+++ test/dotmenu.Tests/Extensions/MenuBuilder/AddOptionTests.cs
@@ -0,0 +1,41 @@
+﻿namespace dotmenu.Tests.Extensions;
+
+public class AddOptionTests
+{
+    [Fact]
+    public void AddOption_WithNullMenu_ThrowsArgumentNullException()
+    {
+        IMenuBuilder? builder = null;
+        Assert.Throws<ArgumentNullException>(TestAction);
+        return;
+        
+        void TestAction() =>
+            builder!.AddOption("text");
+    }
+    
+    [Theory]
+    [InlineData(null!)]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("\t")]
+    public void AddOption_WithInvalidText_ThrowsArgumentException(string? text)
+    {
+        var builder = Menu.CreateDefaultBuilder();
+        Assert.Throws<ArgumentException>(TestAction);
+        return;
+        
+        void TestAction() =>
+            builder.AddOption(text!);
+    }
+    
+    [Fact]
+    public void AddOption_WithValidText_AddsOptionToMenu()
+    {
+        var menu = Menu
+            .CreateDefaultBuilder()
+            .AddOption("sample")
+            .Build();
+        
+        Assert.Single(menu.Elements);
+    }
+}
